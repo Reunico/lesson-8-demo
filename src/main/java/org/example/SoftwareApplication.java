ec638bdfeb89beb61ebfa94c695549ae1005aaf4
andrey <abobrov@reunico.com>
2020-07-31 13:00:33 +0300

    initial commit for lesson 8 demo project with delegates code, listeners and external task

---
+++ src/main/java/org/example/SoftwareApplication.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Software Application")
+public class SoftwareApplication extends ServletProcessApplication {
+}
